25ed510fa61b3a6c057ff8295450da690cb41e50
Olek <olek.dytko@gmail.com>
2019-03-18 22:42:07 +0100

    Added - allChoroby

--- src/main/java/pwr/thesis/thesis/Controller/ChorobyController.java
+++ src/main/java/pwr/thesis/thesis/Controller/ChorobyController.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import pwr.thesis.thesis.Model.Choroby;
 import pwr.thesis.thesis.Repository.ChorobyRepository;
+
+import java.util.List;
 
 @Controller
 public class ChorobyController {
@@ -20,15 +21,22 @@ public class ChorobyController {
     }
 
     @GetMapping("/")
-    public String choroba(Model model){
-        model.addAttribute("choroby",new Choroby());
+    public String choroba(Model model) {
+        model.addAttribute("choroby", new Choroby());
         return "dodajChorobe";
     }
 
     @PostMapping("save")
-    public String addChoroba(Choroby choroby){
+    public String addChoroba(Choroby choroby) {
         chorobyRepository.save(choroby);
         return "success";
+    }
+
+    @GetMapping("/allChoroby")
+    public String allChoroby(Model model) {
+        List<Choroby>allChoroby=chorobyRepository.findAll();
+        model.addAttribute("choroby", allChoroby);
+        return "allChoroby";
     }
 
 }
