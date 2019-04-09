133d0cb2a71fbeabad8b256fcda71d9d88015900
Olek <olek.dytko@gmail.com>
2019-04-09 21:59:27 +0200

    Modal to log in

--- src/main/java/pwr/thesis/thesis/Controller/LoginController.java
+++ src/main/java/pwr/thesis/thesis/Controller/LoginController.java
@@ -1,14 +1,17 @@
 package pwr.thesis.thesis.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pwr.thesis.thesis.Model.User;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
-        return "loginForm";
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+        return "loginAndRegister";
     }
 
 }
